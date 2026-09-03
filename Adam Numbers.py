3304451d7eb697f49033831132cfdbee895fe18a
nishaant61-prog <nishaant61@gmail.com>
2026-09-03 09:22:11 +0530

    Add functionality to identify Adam numbers
    Implement logic to find Adam numbers within a specified range.

---
+++ Adam Numbers.py
@@ -0,0 +1,7 @@
+for i in range (int(input('enter lower limit:')),int(input('enter uppr limit:'))):
+    g=int(max(str(i)))                                                               #"no carry" rule
+        h=int(str(i)[::-1])                                                          #reversing the number i
+        if i<=h:                      
+            k=int(str(i**2)[::-1])                                                   #reversing the number i**2
+            if h**2==k:
+                print([(i,i**2),(h,k)])
